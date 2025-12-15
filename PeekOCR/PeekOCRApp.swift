11bde6f2b6b3fd7a3d240ee832e0a9eed785e01c
steven <scoaila@proton.me>
2025-12-14 19:00:21 -0500

    Initial Commit

---
+++ PeekOCR/PeekOCRApp.swift
@@ -0,0 +1,17 @@
+//
+//  PeekOCRApp.swift
+//  PeekOCR
+//
+//  Created by Steven on 14/12/25.
+//
+
+import SwiftUI
+
+@main
+struct PeekOCRApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
